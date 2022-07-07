3666e6b313e1e4dc93bb38693310dcf011c4409b
Alexandr Dunaev <ghost74rus@gmail.com>
2022-07-07 23:16:19 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+   $('.hero__slider-container').bxSlider({
+      pager: true,
+      controls: true,
+      mode: 'fade',
+      auto: true,
+      pause: 4500,
+      touchEnabled: false
+   });
+});
+
+$(document).ready(function () {
+   $('.team__slider-container').bxSlider({
+      pager: true,
+      controls: true,
+      mode: 'fade',
+      auto: true,
+      pause: 4500,
+      touchEnabled: false
+   });
+});
+
+$(document).ready(function () {
+   $('.sertificates__slider-container').bxSlider({
+      pager: true,
+      controls: true,
+      mode: 'horizontal',
+      auto: true,
+      pause: 4500,
+      touchEnabled: false
+   });
+});
